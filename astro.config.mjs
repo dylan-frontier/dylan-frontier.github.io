c609ca8ca0d4473f1bb44ca8db7efd9f7524de64
dylan-frontier <dylan.balagtas01@gmail.com>
2026-04-03 18:31:12 +0800

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://dylan-frontier.github.io',
+	base: '/dylan-frontier.github.io'
 	integrations: [mdx(), sitemap()],
 });
